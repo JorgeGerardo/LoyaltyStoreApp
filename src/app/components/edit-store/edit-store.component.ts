92404b1dc6560338184b2255d83cb3c1a8a1d077
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-21 09:46:44 -0700

    Update store data added

--- src/app/components/edit-store/edit-store.component.ts
+++ src/app/components/edit-store/edit-store.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { StoreUpdateDto } from 'src/app/api/models';
+import { StoreService } from 'src/app/api/services/store.service';
 
 @Component({
   selector: 'app-edit-store',
   templateUrl: './edit-store.component.html',
 })
-export class EditStoreComponent {
+export class EditStoreComponent implements OnInit{
+  storeId = 0;
+  dto:StoreUpdateDto | undefined = undefined;
 
+  constructor(activated:ActivatedRoute, private store:StoreService) {
+    activated.paramMap.subscribe(p => {
+      this.storeId = parseInt(p.get('storeId') ?? '0', 10);
+    })
+  }
+
+
+  ngOnInit(): void {
+    this.store.getById(this.storeId).subscribe(v => this.dto = v)
+  }
+
+  updateStore(){
+    if (this.dto === undefined)
+      return;
+
+    this.store.update(this.storeId, this.dto).subscribe({
+      next: () => alert('Operación realizada con éxito'),
+      error: (er:HttpErrorResponse) => alert('Ha ocurrido un error ' +er.error)
+    })
+  }
 }

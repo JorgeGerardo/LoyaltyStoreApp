6bb2d5fb1528eea2455bbc16a3ee07b9c5c6ede5
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-20 23:20:55 -0700

    Product viewer path added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { ProductViewerComponent } from './components/product-viewer/product-viewer.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'login'},
   {path: 'login', component: LoginComponent},
+  {path: 'product-viewer', component: ProductViewerComponent},
 ];
 
 @NgModule({

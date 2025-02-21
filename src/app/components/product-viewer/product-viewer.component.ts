ccd4530bcb9965af2f7db04bd8ea8fb4a4455d79
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-21 00:30:33 -0700

    Product viewer template added

--- src/app/components/product-viewer/product-viewer.component.ts
+++ src/app/components/product-viewer/product-viewer.component.ts
@@ -1,39 +1,45 @@
-import { Component } from '@angular/core';
-import { StoreService } from 'src/app/api/services/store.service';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/api/models';
+import { ProductService } from 'src/app/api/services/product.service';
 
 @Component({
   selector: 'app-product-viewer',
   templateUrl: './product-viewer.component.html',
   styleUrls: ['./product-viewer.component.scss']
 })
-export class ProductViewerComponent {
-  constructor(private store:StoreService) { }
+export class ProductViewerComponent implements OnInit{
+  products:Product[] = [];
+  constructor(private store:ProductService) { }
+
+  ngOnInit(): void {
+    this.store.getAll().subscribe(products => this.products = products);
+  }
 
   get(){
     this.store.getAll().subscribe(c => console.log(c));
   }
 
   post(){
-    this.store.post({
-      address: '444',
-      sucursal: "Walmart 444",
-    }).subscribe(console.log);
+    // this.store.post({
+    //   address: '444',
+    //   sucursal: "Walmart 444",
+    // }).subscribe(console.log);
   }
 
   update(){
-    this.store.update(2, {
-      address: 'Santa Catarina, NL',
-      sucursal: "Walmart 222",
-      id: 2
-    }).subscribe(v => console.log(v));
+    // this.store.update(2, {
+    //   address: 'Santa Catarina, NL',
+    //   sucursal: "Walmart 222",
+    //   id: 2
+    // }).subscribe(v => console.log(v));
   }
 
   getById(){
-    this.store.getById(2).subscribe(v => {
-      console.log(v.id);
-      console.log(v.address);
-      console.log(v.sucursal);
-    });
+    // this.store.getById(2).subscribe(v => {
+    //   console.log(v.id);
+    //   console.log(v.address);
+    //   console.log(v.sucursal);
+    // });
   }
 
   count(){

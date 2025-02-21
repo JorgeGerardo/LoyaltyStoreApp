5b59f7144b0251b538b57e917d18d5571b978935
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-21 07:17:40 -0700

    Now can link to store-inventary-edit

--- src/app/components/edit-inventary/edit-inventary.component.ts
+++ src/app/components/edit-inventary/edit-inventary.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs';
 import { StoreInventary } from 'src/app/api/models';
+import { ProductService } from 'src/app/api/services/product.service';
 import { StoreInventaryService } from 'src/app/api/services/store-inventary.service';
 
 @Component({
@@ -11,8 +13,10 @@ import { StoreInventaryService } from 'src/app/api/services/store-inventary.serv
 export class EditInventaryComponent implements OnInit{
   stockInventaryId = 0;
   Inventary:StoreInventary | undefined = undefined
+  productImage = "";
+  productTitle = "No title";
 
-  constructor(private inventary:StoreInventaryService, private activatedRoute:ActivatedRoute) {
+  constructor(private inventary:StoreInventaryService, private activatedRoute:ActivatedRoute, private productS:ProductService) {
     this.activatedRoute.paramMap.subscribe(v => {
       let stockInventaryId = v.get('storeId');
       this.stockInventaryId = parseInt(stockInventaryId ?? '0' ,10);
@@ -20,7 +24,14 @@ export class EditInventaryComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    this.inventary.getById(this.stockInventaryId)
+    this.inventary.getById(this.stockInventaryId).pipe(
+      tap((v) => {
+        this.productS.getById(v.id).subscribe(v => {
+          this.productTitle = v.title;
+          this.productImage = v.image
+        })
+      })
+    )
       .subscribe(data => {
         this.Inventary = data;
         console.log(data);

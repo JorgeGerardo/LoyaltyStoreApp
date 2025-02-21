919f7fcf3d8927570996cd886d32a01a2bd6ca80
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-21 00:11:21 -0700

    Guard auth added

---
+++ src/app/Guard/login-active.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { TokenService } from '../services/token.service';
+
+export const loginActiveGuard: CanActivateFn = (route, state) => {
+  let tokenService = inject(TokenService);
+  let router = inject(Router);
+
+  let token = tokenService.getToken();
+  if (!tokenService.isTokenExpired(token))
+    return true;
+  
+  router.navigate(['/login']);
+  return false;
+};

d3d9f4ed3ef3db4812ba29e29db64aa011b56485
Jorge Gerardo <jorgegerardo0105@gmail.com>
2025-02-21 00:12:24 -0700

    Protect path with guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ProductViewerComponent } from './components/product-viewer/product-viewer.component';
+import { loginActiveGuard } from './Guard/login-active.guard';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'login'},
   {path: 'login', component: LoginComponent},
-  {path: 'product-viewer', component: ProductViewerComponent},
+  {path: 'product-viewer', component: ProductViewerComponent, canActivate: [loginActiveGuard]},
+  {path: '**', pathMatch:'full', redirectTo: 'login'},
 ];
 
 @NgModule({
